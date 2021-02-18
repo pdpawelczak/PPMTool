3c806fb14946a3839f7cc7b1c7ef70316e9bbc22
unknown <pdpawelczak@gmail.com>
2021-02-18 15:11:35 +0100

    Project and header component

--- ppmtool-react/src/App.js
+++ ppmtool-react/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
     <div className="App">
-    <Dashboard />
+      <Header />
+      <Dashboard />
     </div>
   );
 }
