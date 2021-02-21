b677cf034b220bd6eacfa848bdcb340f595cb774
unknown <pdpawelczak@gmail.com>
2021-02-21 13:18:22 +0100

    get project - redux

--- ppmtool-react/src/components/Dashboard.js
+++ ppmtool-react/src/components/Dashboard.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
 import CreateProjectButton from './Project/CreateProjectButton';
 import ProjectItem from './Project/ProjectItem';
+import {connect} from "react-redux";
+import { getProjects} from "../actions/projectActions";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
+
+    componentDidMount(){
+        this.props.getProjects();
+    }
     render() {
         return (
             <div className="projects">
@@ -22,4 +29,13 @@ class Dashboard extends Component {
         )
     }
 }
-export default Dashboard;
+Dashboard.propTypes = {
+    project: PropTypes.object.isRequired,
+    getProjects: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    project: state.project,
+})
+
+export default connect(null, {getProjects})(Dashboard);
