a0cf48a119801564a0fee397effb71cbc948806f
unknown <pdpawelczak@gmail.com>
2021-03-11 22:15:05 +0100

    delete project task

--- ppmtool-react/src/actions/backlogActions.js
+++ ppmtool-react/src/actions/backlogActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK } from "./types";
+import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK, DELETE_PROJECT_TASK } from "./types";
 
 export const addProjectTask = (backlog_id, project_task, history) => async dispatch => {
     try {
@@ -66,4 +66,14 @@ export const cleanErrorProjectTask = () => async dispatch => {
         type: GET_ERRORS,
         payload: {}
     });
+};
+
+export const deleteProjectTask = (backlog_id, pt_id) => async dispatch => {
+    if(window.confirm(`Do you want to delete this project task?`)){
+        await axios.delete(`/api/backlog/${backlog_id}/${pt_id}`)
+        dispatch({
+            type: DELETE_PROJECT_TASK,
+            payload: pt_id
+        });
+    }
 };
